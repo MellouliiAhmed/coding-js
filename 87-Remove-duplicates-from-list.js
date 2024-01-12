8c08d3d173b874c5d27a03704d6ce2b5d57da03a
ahmedmellouli <ahmed.mellouli@esprit.tn>
2024-01-12 08:40:53 -0800

    update Day 9

---
+++ 87-Remove-duplicates-from-list.js
@@ -0,0 +1,30 @@
+// Remove duplicates from list
+
+// Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// Examples:
+
+// Input -> Output
+// [1, 1, 2] -> [1, 2]
+// [1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+
+// function distinct(a) {
+//     let result = []
+//     for(let i=0; i<a.length; i++){
+//         if(result.indexOf(a[i]) == -1){
+//             result.push(a[i]);
+//         }
+//     }
+//     return result;
+//   }
+
+// method 2
+
+
+function distinct(a) {
+    return [...new Set(a)]
+  }
+
+  console.log(distinct([1,1,1,2,3,3,3,5,5,5,55]));
